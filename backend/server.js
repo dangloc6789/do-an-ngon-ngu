6b0999191df2a711eca3ff2bbf8cdc8cc1172b64
dangloc6789 <83728109+dangloc6789@users.noreply.github.com>
2022-02-27 18:51:48 +0700

    điền nội dung commit vào đây

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
+require('dotenv').config();
 const express = require('express');
 const app=express();
 const PORT=process.env.PORT||5000;
-app.listen(PORT,()=>console.log('Server running on port ${PORT}'))
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
